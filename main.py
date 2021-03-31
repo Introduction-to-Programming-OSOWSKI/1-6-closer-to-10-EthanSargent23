2410d468dad91eaa0dfb822b693383bf4a1e1ac6
Sargent Ethan <817233@stma.k12.mn.us>
2021-03-31 10:26:14 -0500

    Close to 10 try1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#WRITE YOUR CODE IN THIS FILE
+def close10(x, y):
+
+    if abs(10 - x):
+        return 
+
+    elif ():
+        return 
+
+    else (x = y):
+        return 0
+
+print(close10(12, 8))
+
